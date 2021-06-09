6723dc8bab614c696156512fba3f0d5633e5ba43
Matt Wynne <matt@cucumber.io>
2021-06-09 11:57:45 -0700

    We finished. We did a retro.

---
+++ RETRO.md
@@ -0,0 +1,6 @@
+What did you think?
+
+    * Really liked VS Code Live share (once it stopped crashing)
+        * All the hands on the keyboard
+    * Mobtime we like
+    * Haskelling went well today. Focussed on actually coding and not faffing with environment setup.
--- life.hs
+++ life.hs
@@ -1,10 +1,17 @@
 import Test.HUnit
 
-next wasAlive neighbors = wasAlive || False
+next :: Bool -> Int -> Bool
+next wasAlive neighbors = neighbors == 2 || neighbors == 3 || False
 
 tests = TestList 
     [
-        TestCase (assertEqual "dead cell with no friends stay dead" (False) (next False 0) )
+        TestCase (assertEqual "dead cell with no friends stay dead" (False) (next False 0) ),
+        TestCase (assertEqual "live cell with no friends becomes dead" (False) (next True 0) ),
+        TestCase (assertEqual "live cell with two friends stay alive" (True) (next True 2) ),
+        TestCase (assertEqual "live cell with three friends stay alive" (True) (next True 3) ),
+        TestCase (assertEqual "live cell with more than three friends dies" (False) (next True 4) ),
+        TestCase (assertEqual "live cell with more less than two friends dies of loneliness" (False) (next True 1) ),
+        TestCase (assertEqual "a dead cell with exactly three friends comes to life" (True) (next False 3) )
     ]
 
 main = do runTestTT tests
